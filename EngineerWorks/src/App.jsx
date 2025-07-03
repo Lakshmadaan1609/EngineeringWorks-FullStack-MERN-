a862dc4b21236bd2788e79f0e2404934761adb08
Laksh Madaan <madaanlaksh2003@gmail.com>
2025-07-03 11:35:31 +0530

    \added dependency

---
+++ EngineerWorks/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./pages/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
+
+
+/*
+cool, lets first create a components for navbar and navItems
+ should be - Home Icon rather Written home, our products, About Us, 
+ Our BLog, Catalogue, contact And a button for downloading brouchour on the right 
+white with black text and add a good animations to it 
+*/
